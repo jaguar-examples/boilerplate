826620e0b1cff587c3fc3fe43d2461e2dbbfaf8b
Ravi Teja Gudapati <tejainece@gmail.com>
2017-07-12 13:44:07 +0200

    jaguar 0.6.x

--- bin/server.dart
+++ bin/server.dart
@@ -1,13 +1,10 @@
 import 'package:jaguar/jaguar.dart';
-import 'package:jaguar_reflect/jaguar_reflect.dart';
 
 import 'package:boilerplate/api.dart';
 
 main(List<String> args) async {
-  final ea = new JaguarReflected(new ExampleApi());
+  final server = new Jaguar(multiThread: true);
+  server.addApiReflected(new ExampleApi());
 
-  Jaguar configuration = new Jaguar(multiThread: true);
-  configuration.addApi(ea);
-
-  await configuration.serve();
+  await server.serve();
 }
